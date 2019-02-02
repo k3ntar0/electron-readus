bf6a0a6f28ef638d5af242d94cb808d2443de688
k3ntar0 <k3ntar01wata@gmail.com>
2019-02-02 13:45:29 +0900

    fileUtil.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const electron = require ('electron');
-const remote = electron.remote;
-const fileUtil = remote.require('./lib/fileUtil');
+var electron = require('electron');
+var remote = electron.remote;
+var fileUtil = remote.require('./lib/fileUtil');
+var baseDir = process.cwd();
 
-fileUtil.fetchReadmeList(function(err, matches) {
-  if(!err) document.write(matches.join());
+var ngModule = angular.module('readUs', []);
+
+ngModule.controller('MainController', function ($scope) {
+  var main = this;
+
+  // Get README.md
+  main.getFile = function(file) {
+    main.fileText = fileUtl.getAsText(file.filepath);
+  };
+
+  fileUtil.fetchReadmeList(baseDir, function (err, fileList) {
+      if(err) console.error(err);
+      $scope.$apply(function () {
+        main.fileList = fileList;
+      });
+  });
+});
+
+ngModule.directive('mdPreview', function () {
+  return function ($scope, $elem, $attrs) {
+    $scope.$watch($attrs.mdPreview, function(source) {
+      $elem.html(marked(source));
+    });
+  };
 });
